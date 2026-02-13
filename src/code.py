23e69131260a0096c12100c9ff940c2b2af8eb71
Jev <jev.kuznetsov@gmail.com>
2026-02-13 16:28:50 +0100

    integrate clock into main

--- src/code.py
+++ src/code.py
@@ -1,5 +1,8 @@
 import time
+
+import adafruit_ds3231  # type: ignore
 import board
+import busio
 import digitalio
 
 
@@ -8,10 +11,14 @@ def main() -> None:
     led = digitalio.DigitalInOut(board.LED)
     led.direction = digitalio.Direction.OUTPUT
 
+    i2c = busio.I2C(board.GP1, board.GP0)
+    rtc = adafruit_ds3231.DS3231(i2c)
+
     while True:
         led.value = not led.value
-        time.sleep(0.5)
-
+        t = rtc.datetime
+        print(f"{t.tm_hour:02d}:{t.tm_min:02d}:{t.tm_sec:02d}")
+        time.sleep(1.0)
 
 
 main()
